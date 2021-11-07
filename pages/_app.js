3f62dbd3d80525284883ae9e60584ca7c1050ae2
tanida.kazuki <tanida.kazuki@moneyforward.co.jp>
2021-11-07 20:10:52 +0900

    remove console.log

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,6 @@
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
-  console.log("component >>>", Component)
-  console.log("pageProps >>>", pageProps)
-
   return <Component {...pageProps} />
 }
 
